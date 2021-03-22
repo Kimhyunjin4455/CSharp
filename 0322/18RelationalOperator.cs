e6b1adff3a96168c3e63044a3f2052542fe88548
Kimhyunjin4455 <80228088+Kimhyunjin4455@users.noreply.github.com>
2021-03-22 11:12:25 +0900

    Add files via upload

---
+++ 0322/18RelationalOperator.cs
@@ -0,0 +1,32 @@
+using System;
+
+
+namespace Basic
+{
+    class Overflow
+
+
+
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int first = 10, second = 20;
+            result = (first == second);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+            result = (first > second);
+            Console.WriteLine("{0} > {1} : {2}", first, second, result);
+            result = (first < second);
+            Console.WriteLine("{0} < {1} : {2}", first, second, result);
+            result = (first >= second);
+            Console.WriteLine("{0} >= {1} : {2}", first, second, result);
+            result = (first <= second);
+            Console.WriteLine("{0} <= {1} : {2}", first, second, result);
+            result = (first != second);
+            Console.WriteLine("{0} != {1} : {2}", first, second, result);
+
+
+
+        }
+    }
+}
